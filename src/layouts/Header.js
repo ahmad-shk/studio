47a3a7d6ed11cc61e2761b936e9c9cd8d1e91df9
ahmad-shk <ahmadsaleem19950@gmail.com>
2024-12-07 04:01:51 +0500

    update

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const handleWhatsAppClick = () => {
-    const phoneNumber = "+923173070894"; 
-    const message = "Hello! I need assistance."; 
-    const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
-    window.open(whatsappURL, "_blank");
-  };
   return (
     <header className="header py-6 px-8 flex justify-between items-center text-white bg-[#00000087] fixed top-0 z-[5] w-full">
       <div className="logo">
         <img src="../../../logo.png" alt="Logo" />
       </div>
-      <nav className="nav text-[#C7C7C7] flex gap-12 hover:[&>*]:text-[white] -font-bold max-[1200px]:hidden">
+      <nav className="nav text-[#C7C7C7] flex gap-12 hover:[&>*]:text-[white] max-[1200px]:hidden">
         <Link to="/">Home</Link>
         <Link to="/news">News</Link>
         <Link to="/shows">Shows</Link>
@@ -25,7 +19,6 @@ function Header() {
       <div className="menu-icon">
       <img src="../../../menuB.png" alt="Logo" />
       </div>
-
     </header>
   );
 }
